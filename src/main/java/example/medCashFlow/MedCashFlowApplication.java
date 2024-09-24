fdcb50725c14228d1ea05077b79a945c636babc8
FernandoJunior7 <fernando.junior@edu.unifil.br>
2024-09-24 12:44:47 -0300

    Inital structure of the project

---
+++ src/main/java/example/medCashFlow/MedCashFlowApplication.java
@@ -0,0 +1,13 @@
+package example.medCashFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCashFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedCashFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/medCashFlow/MedCashFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package example.medCashFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedCashFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
