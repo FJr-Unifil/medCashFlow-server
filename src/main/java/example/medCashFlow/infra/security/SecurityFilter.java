c520c25cebcc5075e13f3c06cab5cd3a8b8250c3
FernandoJunior7 <fernando.junior@edu.unifil.br>
2024-11-24 21:43:41 -0300

    Fix: not recovering admin token

--- src/main/java/example/medCashFlow/infra/security/SecurityFilter.java
+++ src/main/java/example/medCashFlow/infra/security/SecurityFilter.java
@@ -7,9 +7,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -21,18 +24,28 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
 
-    private final EmployeeService employeeService;
-    
+    private final ApplicationContext applicationContext;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
 
         if (token != null) {
-            var email = tokenService.validateToken(token);
-            UserDetails employee = employeeService.getEmployeeByEmail(email);
+            var username = tokenService.validateToken(token);
+            UserDetails user = null;
 
-            var authentication = new UsernamePasswordAuthenticationToken(employee, null, employee.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            try {
+                InMemoryUserDetailsManager inMemoryManager = applicationContext.getBean(InMemoryUserDetailsManager.class);
+                user = inMemoryManager.loadUserByUsername(username);
+            } catch (UsernameNotFoundException e) {
+                EmployeeService employeeService = applicationContext.getBean(EmployeeService.class);
+                user = employeeService.getEmployeeByEmail(username);
+            }
+
+            if (user != null) {
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
         }
 
         filterChain.doFilter(request, response);
