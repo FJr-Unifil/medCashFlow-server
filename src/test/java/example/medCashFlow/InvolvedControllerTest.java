23ccb97c024064bb74055ef99dc20b7e6a76bdde
FernandoJunior7 <fernando.junior@edu.unifil.br>
2024-11-27 09:51:09 -0300

    Fix: InvoledController test connected to the prod db

--- src/test/java/example/medCashFlow/InvolvedControllerTest.java
+++ src/test/java/example/medCashFlow/InvolvedControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -35,6 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class InvolvedControllerTest {
 
     @Autowired
