0d16a26c6d482b87600acb1ace86ddb249a0e85c
FJr-Unifil <fernando.junior@edu.unifil.br>
2025-05-10 18:57:12 -0300

    Implement multiple invalid fields for involveds

--- src/main/java/example/medCashFlow/services/InvolvedService.java
+++ src/main/java/example/medCashFlow/services/InvolvedService.java
@@ -2,7 +2,8 @@ package example.medCashFlow.services;
 
 import example.medCashFlow.dto.involved.InvolvedRegisterDTO;
 import example.medCashFlow.dto.involved.InvolvedResponseDTO;
-import example.medCashFlow.exceptions.InvalidDataException;
+import example.medCashFlow.exceptions.ApiValidationError;
+import example.medCashFlow.exceptions.MultipleInvalidDataException;
 import example.medCashFlow.exceptions.ResourceNotFoundException;
 import example.medCashFlow.mappers.InvolvedMapper;
 import example.medCashFlow.model.Clinic;
@@ -11,8 +12,7 @@ import example.medCashFlow.repository.InvolvedRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -42,35 +42,35 @@ public class InvolvedService {
                 .map(mapper::toResponseDTO).toList();
     }
 
-    public boolean isInvolvedValid(String document, String email) {
-        return (isInvolvedValidByDocument(document) && isInvolvedValidByEmail(email));
-    }
+    public Map<String, String> getInvalidFields(InvolvedRegisterDTO data) {
+        Map<String, String> invalidFields = new HashMap<>();
 
-    private boolean isInvolvedValidByDocument(String document) {
-        if (repository.existsByDocument(document)) {
-            throw new InvalidDataException(
-                    Involved.class.getSimpleName(),
-                    "document",
-                    document
-            );
+        if (repository.existsByDocument(data.document())) {
+            invalidFields.put("document", data.document());
         }
-        return true;
-    }
 
-    private boolean isInvolvedValidByEmail(String email) {
-        if (repository.existsByEmail(email)) {
-            throw new InvalidDataException(
-                    Involved.class.getSimpleName(),
-                    "email",
-                    email
-            );
+        if (repository.existsByEmail(data.email())) {
+            invalidFields.put("email", data.email());
         }
-        return true;
+
+        if (repository.existsByPhone(data.phone())) {
+            invalidFields.put("phone", data.phone());
+        }
+
+        return invalidFields;
     }
 
     public InvolvedResponseDTO createInvolved(InvolvedRegisterDTO data, Clinic clinic) {
-        if (!isInvolvedValid(data.document(), data.email())) {
-            return null;
+        List<ApiValidationError> errors = new ArrayList<>();
+        Map<String, String> invalidFields = getInvalidFields(data);
+
+        if (!invalidFields.isEmpty()) {
+            invalidFields.forEach((key, value) -> errors.add(new ApiValidationError(
+                    Involved.class.getSimpleName(),
+                    key,
+                    value
+            )));
+            throw new MultipleInvalidDataException(errors);
         }
 
         Involved involved = mapper.toInvolved(data, clinic);
@@ -79,25 +79,36 @@ public class InvolvedService {
         return mapper.toResponseDTO(involved);
     }
 
-    public InvolvedResponseDTO updateInvolved(InvolvedRegisterDTO data, Clinic clinic, Long id) {
+    public InvolvedResponseDTO updateInvolved(InvolvedRegisterDTO data, Long id) {
+        List<ApiValidationError> errors = new ArrayList<>();
         Involved existingInvolved = getInvolvedById(id);
 
-        if (!data.email().equals(existingInvolved.getEmail())
-                && repository.existsByEmail(data.email())) {
-            throw new InvalidDataException(
-                    Involved.class.getSimpleName(),
-                    "email",
-                    data.email()
-            );
-        }
-
-        if (!data.document().equals(existingInvolved.getDocument())
-                && repository.existsByDocument(data.document())) {
-            throw new InvalidDataException(
+        if (repository.existsByDocumentAndIdNot(data.document(), id)) {
+            errors.add(new ApiValidationError(
                     Involved.class.getSimpleName(),
                     "document",
                     data.document()
-            );
+            ));
+        }
+
+        if (repository.existsByEmailAndIdNot(data.email(), id)) {
+            errors.add(new ApiValidationError(
+                    Involved.class.getSimpleName(),
+                    "email",
+                    data.email()
+            ));
+        }
+
+        if (repository.existsByPhoneAndIdNot(data.phone(), id)) {
+            errors.add(new ApiValidationError(
+                    Involved.class.getSimpleName(),
+                    "phone",
+                    data.phone()
+            ));
+        }
+
+        if (!errors.isEmpty()) {
+            throw new MultipleInvalidDataException(errors);
         }
 
         mapper.updateInvolved(existingInvolved, data);
