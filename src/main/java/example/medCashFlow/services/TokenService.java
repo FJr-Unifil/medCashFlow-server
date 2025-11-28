8e64f1beebf57e1d6865713cb20c6ec3fe36a132
FJr-Unifil <fernando.junior@edu.unifil.br>
2025-11-28 18:46:47 -0300

    refactor: consolidate duplicate token generation methods
    Remove generateTokenForAdmin() method and update generateToken() to
    accept UserDetails interface, eliminating code duplication

--- src/main/java/example/medCashFlow/services/TokenService.java
+++ src/main/java/example/medCashFlow/services/TokenService.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import example.medCashFlow.model.Employee;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,31 +21,14 @@ public class TokenService {
     @Value("${api.security.token.secret:my-secret-key}")
     private String secret;
 
-    public String generateToken(Employee employee) {
+    public String generateToken(UserDetails userDetails) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
 
             return JWT.create()
                     .withIssuer(ISSUER)
-                    .withSubject(employee.getEmail())
-                    .withClaim("roles", employee.getAuthorities().stream()
-                            .map(GrantedAuthority::getAuthority)
-                            .toList())
-                    .withExpiresAt(generateExpirationDate())
-                    .sign(algorithm);
-        } catch (JWTCreationException exception) {
-            throw new RuntimeException("Error while generating token", exception);
-        }
-    }
-
-    public String generateTokenForAdmin(UserDetails admin) {
-        try {
-            Algorithm algorithm = Algorithm.HMAC256(secret);
-
-            return JWT.create()
-                    .withIssuer(ISSUER)
-                    .withSubject(admin.getUsername())
-                    .withClaim("roles", admin.getAuthorities().stream()
+                    .withSubject(userDetails.getUsername())
+                    .withClaim("roles", userDetails.getAuthorities().stream()
                             .map(GrantedAuthority::getAuthority)
                             .toList())
                     .withExpiresAt(generateExpirationDate())
