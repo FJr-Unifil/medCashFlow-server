7a713836038c94bb714a3b80bfbeaef9edd77f15
FJr-Unifil <fernando.junior@edu.unifil.br>
2025-12-23 23:26:03 -0300

    droid testing

--- src/main/java/example/medCashFlow/exceptions/CustomExceptionHandler.java
+++ src/main/java/example/medCashFlow/exceptions/CustomExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.security.authentication.InternalAuthenticationService
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
+import org.springframework.security.access.AccessDeniedException;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
